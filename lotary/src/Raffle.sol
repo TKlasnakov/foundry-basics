71367b98f8df6c7b6b7cd2722eb1ae9b1c3ccd1e
tklasnakov <t.klasnakov@gmail.com>
2025-01-26 10:25:06 +0200

    fix git ignore and add enter player functionality in the Raffle sol

--- lotary/src/Raffle.sol
+++ lotary/src/Raffle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-
 /**
 * @title Raffle
 * @dev Implement Chainlink VRF
@@ -9,23 +8,40 @@ pragma solidity ^0.8.18;
 * @notice Create a simple Raffle
 */
 contract Raffle {
-	uint256 private immutable i_entraceFee;
 	error Raffle__SendMoreToEnterRaffle();
+	error Raffle_NotEnoughTimeHasPassed();
+	
+	uint256 private immutable i_entraceFee;
+	uint256 private immutable i_interval;
+	uint256 private s_lastTimeStamp;
+	address payable[] private s_players;
 
-	constructor (uint256 _entraceFee) {
+	event RaffleEntered(address indexed player);
+
+	constructor (uint256 _entraceFee, uint256 _interval) {
 		i_entraceFee = _entraceFee;
+		i_interval = _interval;
+		s_lastTimeStamp = block.timestamp;
 	}
 
 	function enterRaffle() external payable {
 		if (msg.value < i_entraceFee) {
 			revert Raffle__SendMoreToEnterRaffle();
 		}
+
+		s_players.push(payable(msg.sender));
+		emit RaffleEntered(msg.sender);
 	}	
 
-	function pickWinner() public {}
+	function pickWinner() external view {
+		if(block.timestamp - s_lastTimeStamp < i_interval) {
+			revert Raffle_NotEnoughTimeHasPassed();
+		}
+	}
 
 
 	function getEntranceFee() external view returns (uint256) {
 		return i_entraceFee;
 	}
+
 }
